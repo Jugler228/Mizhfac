c7358998087dd258ea50d5208e4f49a20ceb7bcc
Danbug <you@example.comyou@example.com>
2022-09-27 10:57:11 +0300

    Fixed Battle class. Implemented tests

--- src/main/java/org/pupa/Battle.java
+++ src/main/java/org/pupa/Battle.java
@@ -9,7 +9,7 @@ public class Battle {
         while(warrior1.isAlive() && warrior2.isAlive()){
             warrior2.receiveDamage(warrior1);
             if(warrior2.isAlive()){
-                warrior1.receiveDamage(warrior1);
+                warrior1.receiveDamage(warrior2);
             }
         }
         return warrior1.isAlive();
